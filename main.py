9e9a1225a255bebc024d05ef686c2891cfde9f4f
Volodymyr Semenov <vandorlot@gmail.com>
2023-02-28 20:52:47 -0500

    Started Flask Project Structure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import flask
 import socketserver
 from pymongo import MongoClient
 
@@ -7,21 +8,32 @@ db = mongo_client["CSE312-Group-Project-Test"]
 master = db["Testing"]
 
 
-class MyTCPHandler(socketserver.BaseRequestHandler):
-    socketserver.TCPServer.allow_reuse_address = True
-
-    def handle(self):
-
-        received_data = self.request.recv(2048)
-        dataInsert = {"Name": "Journey"}
-        master.insert_one(dataInsert)
-        self.request.sendall(
-            f'HTTP/1.1 200 OK\r\nContent-Length: {len("Hello")}\r\nContent-Type: text/plain; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\n\r\nHello'.encode()
-        )
+app = Flask(__name__)
 
 
-if __name__ == '__main__':
-    host = "0.0.0.0"
-    port = 8000
-    server = socketserver.ThreadingTCPServer((host, port), MyTCPHandler)
-    server.serve_forever()
+@app.route('/')
+def return_html():  # Naming convention can be changed
+    return flask.render_template('index.html')  # It renders the html, pycharm says there is an error
+
+
+# @app.route('/style.css')
+# def return_css():
+#     return flask.render_template('style.css')
+
+# class MyTCPHandler(socketserver.BaseRequestHandler):
+#     socketserver.TCPServer.allow_reuse_address = True
+#
+#     def handle(self):
+#         received_data = self.request.recv(2048)
+#         dataInsert = {"Name": "Journey"}
+#         master.insert_one(dataInsert)
+#         self.request.sendall(
+#             f'HTTP/1.1 200 OK\r\nContent-Length: {len("Hello")}\r\nContent-Type: text/plain; charset=utf-8\r\nX-Content-Type-Options: nosniff\r\n\r\nHello'.encode()
+#         )
+#
+#
+# if __name__ == '__main__':
+#     host = "0.0.0.0"
+#     port = 8000
+#     server = socketserver.ThreadingTCPServer((host, port), MyTCPHandler)
+#     server.serve_forever()
