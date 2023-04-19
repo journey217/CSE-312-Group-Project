28beaf7a6ac441f40a72d7603f363d446f83b649
ksy990719 <ksy990719@hotmail.com>
2023-04-19 17:08:47 -0400

    update addlisting popup

--- frontend/src/components/AddListingPopup.jsx
+++ frontend/src/components/AddListingPopup.jsx
@@ -5,19 +5,21 @@ function AddListingPopup({ onClose, onSubmit }) {
   const [price, setPrice] = useState('');
   const [description, setDescription] = useState('');
   const [condition, setCondition] = useState('');
+  const [category, setCategory] = useState('');
   const [image, setImage] = useState(null);
 
   const handleNameChange = (event) => setName(event.target.value);
   const handlePriceChange = (event) => setPrice(event.target.value);
   const handleDescriptionChange = (event) => setDescription(event.target.value);
   const handleConditionChange = (event) => setCondition(event.target.value);
+  const handleCategoryChange = (event) => setCategory(event.target.value);
   const handleImageChange = (event) => setImage(event.target.files[0]);
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     // Call the onSubmit prop with the form data
-    onSubmit({ name, price, description, condition, image });
+    onSubmit({ name, price, description, condition, category, image });
   };
 
   return (
@@ -39,12 +41,24 @@ function AddListingPopup({ onClose, onSubmit }) {
         <div class="form-field">
           <label>Condition:</label>
           <select value={condition} onChange={handleConditionChange}>
-            <option value="Brand New">Brand New</option>
+            <option value="" disabled hidden>--- Select From Below ---</option>            <option value="Brand New">Brand New</option>
             <option value="Like New">Like New</option>
             <option value="Very Good">Very Good</option>
             <option value="Good">Good</option>
             <option value="Fair">Fair</option>
             <option value="Poor">Poor</option>
+          </select>
+        </div>
+        <div class="form-field">
+          <label>Category</label>
+          <select value={category} onChange={handleCategoryChange}>
+            <option value="" disabled hidden>--- Select From Below ---</option>            <option value="Car Parts">Car Parts</option>
+            <option value="Electronics">Electronics</option>
+            <option value="Home Decor ">Home Decor</option>
+            <option value="Clothing">Clothing</option>
+            <option value="Toys">Toys</option>
+            <option value="Sports">Sports</option>
+            <option value="Appliances">Appliances</option>
           </select>
         </div>
         <div class="form-field">
