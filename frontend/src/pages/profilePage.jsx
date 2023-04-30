39a1ffd146643ad573234f33f4617de1b29fcdd4
ksy990719 <ksy990719@hotmail.com>
2023-04-30 13:46:49 -0400

    Update profilePage.jsx

--- frontend/src/pages/profilePage.jsx
+++ frontend/src/pages/profilePage.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/profilePage.css";
 import BasicProfile from "../assets/Profile_img.png"
 import { Link, useNavigate } from 'react-router-dom';
@@ -15,21 +15,22 @@ export default function Login(page) {
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                if(data.status === 0){
+                if (data.status === 0) {
                     navigate('/login')
                 }
                 else {
                     setImageName(data.user.image)
                     setUsername(data.user.username)
-                    
+                    setAuctionHistory(data.user.auctionHistory)
+                    // setBidHistory(data.user.bidHistory)
                 }
             })
     }, [])
-    return(
+    return (
         <div className="profile_background">
-            <div className="profile_profile_area"> 
+            <div className="profile_profile_area">
                 <img src={imageName}
-                className="profile_profile_area_profileImage"
+                    className="profile_profile_area_profileImage"
                 ></img>
                 <div className="profile_profile_area_name_desc">
                     <h2>{username}</h2>
@@ -38,6 +39,14 @@ export default function Login(page) {
             <div className="profile_bid_history">
                 <div className="profile_bid_container">
                     <h2>Selling list</h2>
+                    {auctionHistory && auctionHistory.map(item => (
+                        <div key={item.id} className="item_row">
+                            <p>Name: {item.name}</p>
+                            <p>Time: {item.endtime}</p>
+                            <p>Status: {item.ongoing}</p>
+                            <p>Price: {item.price}</p>
+                        </div>
+                    ))}
                 </div>
                 <div className="profile_bid_container">
                     <h2>Bids history</h2>
