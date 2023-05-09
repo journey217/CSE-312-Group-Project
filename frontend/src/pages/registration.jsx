5c8b673a6b8d897a4406e8b989ec3248c95098ff
ksy990719 <ksy990719@hotmail.com>
2023-05-09 04:09:13 -0400

    UI UX update

--- frontend/src/pages/registration.jsx
+++ frontend/src/pages/registration.jsx
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import "../styles/registration.css";
 
-let stri = "hi"
 function Register() {
     const [image, setImage] = useState(null);
+    const [imageName, setImageName] = useState('');
+    const [imagePreview, setImagePreview] = useState(null);
     const [errors, setErrors] = useState({
         username: '',
         email: '',
@@ -13,9 +14,24 @@ function Register() {
         submit: '',
         image: ''
     });
+
     const handleImageChange = (event) => {
-        setImage(event.target.files[0]);
+        const file = event.target.files[0];
+        const reader = new FileReader();
+        reader.onloadend = () => {
+            setImage(file);
+            setImageName(file.name);
+            setImagePreview(reader.result);
+        };
+        if (file) {
+            reader.readAsDataURL(file);
+        } else {
+            setImage(null);
+            setImageName('');
+            setImagePreview(null);
+        }
     };
+
     const navigate = useNavigate();
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -60,19 +76,27 @@ function Register() {
                 <div className="register-form">
                     <form className="reg-inputs" onSubmit={handleSubmit}>
                         <header className="formheader">Create New Account</header>
-                        <p>Upload Profile Picture</p>
-                        <input name="image" type="file" onChange={handleImageChange} />
+                        <p>
+                            <input name="image" type="file" id="image" onChange={handleImageChange} className="file-input" />
+                            <label htmlFor="image" className="file-input-label">{imageName || "Upload Profile Picture"}</label>
+                            {imagePreview && (
+                                <div className="upload-preview">
+                                    <img src={imagePreview} alt="Profile" />
+                                </div>
+                            )}
+                        </p>
+
                         {errors.username && <div className="error-message">{errors.username}</div>}
                         <input type="text" placeholder='Username' name='username' required />
                         {errors.email && <div className="error-message">{errors.email}</div>}
                         <input type="email" placeholder="Email" name="email" required />
-                        {errors.password1 && <div className="error-message" dangerouslySetInnerHTML={{ __html: errors.password1}}></div>}
+                        {errors.password1 && <div className="error-message" dangerouslySetInnerHTML={{ __html: errors.password1 }}></div>}
                         <input type="password" placeholder="Password" name="password1" required />
                         {errors.password2 && <div className="error-message">{errors.password2}</div>}
                         <input type="password" placeholder="Confirm Password" name="password2" required />
                         <button className="register-button" type="submit" >Register</button>
                         {errors.submit && <div className="error-message">{errors.submit}</div>}
-                        
+
                     </form>
                     <button className="login-redirect-button" onClick={() => { navigate("/login") }}>
                         Have An Account? Click Here
@@ -83,3 +107,4 @@ function Register() {
     );
 }
 export default Register;
+
